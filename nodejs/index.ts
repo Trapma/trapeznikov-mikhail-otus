f5ecea80cca77b8cf2cc8990dd012fb10975b8fd
Trapma <Trapma@users.noreply.github.com>
2021-06-02 11:16:45 +0300

    count test

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -20,19 +20,19 @@ let test = "src";
 //создадим массивы для записи файлов и папок
 //мне нужна функция которая отсортирует файлы и папки, а затем вернет готовый результат
 
-function root(dir: string) {
+function root(dir: string, calback:any) {
   let arrFile: string[] = [];
   let arrDir: string[] = [];
 
   //счетчик для отслеживания рекурсии
   let countTree = 0;
 
-  async function recFunc(dir: string) {
+  let recFunc = function (dir: string) {
     const copyCountTree = countTree;
     // console.log('test start recFunc');
 
     //читаем папку
-    await fs.readdir(dir, async (err, files) => {
+    fs.readdir(dir, async (err, files) => {
       if (err) {
         throw new Error("READDIR_ERROR");
       }
@@ -42,48 +42,45 @@ function root(dir: string) {
         return console.log({ arrFile, arrDir });
       }
       // console.log(files);
-
-      await files.forEach( (el, i) => {
+     await files.forEach((el, i) => {
         //для каждого файла мы должны создать путь, который будет принимать функция стат
         let pathEl = path.join(dir, el);
         // console.log("проверка счетчиков. CopyCountTree = " + copyCountTree + ", CountTree = " + countTree);
-
         // console.log("files.forEach, el = ", pathEl);
-
-        fs.stat(pathEl, async (err, stats) => {
+        fs.stat(pathEl, (err, stats) => {
           if (err) {
             throw new Error("STAT_ERROR");
           }
 
           if (stats.isFile()) {
             // arrFile = [...arrFile, pathEl];
-            await arrFile.push(pathEl)
-            if (i === files.length - 1 && copyCountTree === countTree) {
-              console.log(`i = ${i}, files.length -1 = ${files.length-1} && copyCountTree = ${copyCountTree}, countTree = ${countTree}`);
+            arrFile.push(pathEl);
+            if ((i === files.length - 1) && (copyCountTree === countTree)) {
+              console.log(`i = ${i}, files.length -1 = ${files.length - 1} && copyCountTree = ${copyCountTree}, countTree = ${countTree}`);
 
               let result = JSON.stringify({ arrFile, arrDir });
-              return console.log(result);
+              calback(result)
 
             }
             // console.log(arrFile);
           }
           if (stats.isDirectory()) {
             // arrDir = [...arrDir, pathEl];
-            await arrDir.push(pathEl)
+            arrDir.push(pathEl);
             countTree++;
-             await recFunc(pathEl)
+            recFunc(pathEl);
           }
         });
       });
     });
   }
-  recFunc(dir);
+
+  recFunc(dir)
   setTimeout(() => {
     console.log('setTimeout',JSON.stringify({ arrFile, arrDir }));
   }, 500);
 }
-root(test);
+root(test, function (result:any) {
+  console.log('test result = ',result);
 
-
-
-module.exports = root;
+});
